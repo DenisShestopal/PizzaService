ca4ef0245a41923942e8a1fa9b57ce61db2e7fdb
Denys_Shestopal <deniskus@bigmir.net>
2016-10-04 13:10:05 +0300

    static Map<Long, Pizza> added to SimpleOrderService

--- src/main/java/SimpleOrderService.java
+++ src/main/java/SimpleOrderService.java
@@ -1,7 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class SimpleOrderService {
+
+    private List<Order> listOrders;
+    private static Map<Long, Pizza> pizzas;
+
+    static{
+        pizzas.put(1L, new Pizza(1L, "Sea", 33.33, PizzaType.Sea));
+        pizzas.put(1L, new Pizza(2L, "Vagatarian", 44.44, PizzaType.Vegetarian));
+        pizzas.put(1L, new Pizza(3L, "Meat", 55.55, PizzaType.Meat));
+    }
+
 
     public Order placeNewOrder(Customer customer, Integer... pizzasId) {
         List<Pizza> pizzas = new ArrayList<>();
