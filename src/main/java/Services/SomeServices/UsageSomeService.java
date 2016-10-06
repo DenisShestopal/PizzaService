71cc00cee79853ccbed8e7929b10ab98f4380ecb
Denys_Shestopal <deniskus@bigmir.net>
2016-10-06 15:24:24 +0300

    UsageSomeService added for contexts testing

---
+++ src/main/java/Services/SomeServices/UsageSomeService.java
@@ -0,0 +1,14 @@
+package Services.SomeServices;
+
+public class UsageSomeService {
+
+    private SomeService someService;
+
+    public UsageSomeService(SomeService someService) {
+        this.someService = someService;
+    }
+
+    public void init(){
+        System.out.println("Call: " + someService.getString());
+    }
+}
--- src/main/java/SpringAppRunner.java
+++ src/main/java/SpringAppRunner.java
@@ -1,6 +1,7 @@
 import Domain.Order;
 import Repository.PizzaRepository;
 import Services.OrderService;
+import Services.SomeServices.SomeService;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,20 +10,30 @@ import java.util.Arrays;
 public class SpringAppRunner {
 
     public static void main(String ... args){
-        ConfigurableApplicationContext context =
-                new ClassPathXmlApplicationContext("appContext.xml");
+        ConfigurableApplicationContext repoContext =
+                new ClassPathXmlApplicationContext("repoContext.xml");
         //указываем, на основе какой конфигурации спринга работаем
 
-        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+        System.out.println(Arrays.toString(repoContext.getBeanDefinitionNames()));
 
-        PizzaRepository pizzaRepository = (PizzaRepository) context.getBean("pizzaRepository");
+        ConfigurableApplicationContext appContext =
+                new ClassPathXmlApplicationContext(
+                        new String[]{"appContext.xml"}, repoContext);
+
+        System.out.println(Arrays.toString(appContext.getBeanDefinitionNames()));
+
+        System.out.println(repoContext.getBean("T1", SomeService.class).getString());
+        System.out.println(appContext.getBean("T1", SomeService.class).getString());
+
+        PizzaRepository pizzaRepository = (PizzaRepository) repoContext.getBean("pizzaRepository");
         System.out.println(pizzaRepository.getPizzaById(1L));
 
-        OrderService orderService = (OrderService) context.getBean("orderService");
+        OrderService orderService = (OrderService) appContext.getBean("orderService");
         Order order = orderService.placeNewOrder(null, 1L, 2L, 3L);
         System.out.println(order);
 
-        context.close();
+        repoContext.close();
+        appContext.close();
 
     }
 }
